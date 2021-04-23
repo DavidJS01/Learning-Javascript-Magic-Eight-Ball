3c20756d864092cbf7dab0df488bc4582faed6b8
David Shipman <53328559+DavidJS01@users.noreply.github.com>
2021-04-22 18:48:54 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+let eightBallNumber = Math.floor(Math.random() * 4);
+let x
+
+switch (eightBallNumber){
+  case 0:
+  x = 'Ask again later'
+  window.alert(x);
+  break
+  case 1:
+  x = 'It is certain'
+  window.alert(x);
+  break
+  case 2:
+  x = 'Probably not'
+  window.alert(x);
+  break
+  case 3:
+  x = "Do not count on it"
+  window.alert(x);
+}
+
